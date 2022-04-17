4c07aa759644475255a1597d5b9f8eb24a710908
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-04-17 20:11:01 -0300

    AdicionadoMetodoPayment

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -10,69 +10,90 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.demo.emtities.Category;
 import com.example.demo.emtities.Order;
+import com.example.demo.emtities.OrderItem;
+import com.example.demo.emtities.Payment;
 import com.example.demo.emtities.Product;
 import com.example.demo.emtities.User;
 import com.example.demo.emtities.enums.OrderStatus;
 import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.repositories.OrderItemRepository;
 import com.example.demo.repositories.OrderRepository;
 import com.example.demo.repositories.ProductRepository;
 import com.example.demo.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
-	
+public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
-	private OrderRepository orderrespository;
+	private OrderRepository orderRepository;
 
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
+
 	@Override
 	public void run(String... args) throws Exception {
 		
-	User u1 = new User(01, "Maria", "maria@gmail.com", "48998473257", "53214");	      //Id null não funcionou<< a verificar"
-	User u2 = new User(02, "Aline", "Aline@gmail.com", "54998473277", "50024");	
+		Category cat1 = new Category(01, "Eletronics");
+		Category cat2 = new Category(02, "Books");
+		Category cat3 = new Category(03, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-	
-	Order o1 = new Order (null,Instant.parse("2021-06-23T18:54:07Z"),OrderStatus.PAID, u1);
-	Order o2 = new Order (null,Instant.parse("2021-06-18T15:47:10Z"),OrderStatus.SHIPPED, u2);	
-	Order o3 = new Order (null,Instant.parse("2021-06-21T05:25:14Z"),OrderStatus.CANCELED, u1);	
-	
-	
-	Category cat1 = new Category(01,"Eletronics");
-	Category cat2 = new Category(02,"Books");
-	Category cat3 = new Category(03,"Computers");
-	
-	Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
-	Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
-	Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
-	Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
-	Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
-	
-	p1.getCategories().add(cat2);
-	p2.getCategories().add(cat1);
-	p2.getCategories().add(cat3);
-	p3.getCategories().add(cat3);
-	p4.getCategories().add(cat3);
-	p5.getCategories().add(cat2);
-	
-	
-    categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
-	productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-	
-	userRepository.saveAll(Arrays.asList(u1,u2));
-  	orderrespository.saveAll(Arrays.asList(o1,o2,o3));
-	
-	productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		
+		User u1 = new User(01, "Maria", "maria@gmail.com", "48998473257", "53214"); 
+		User u2 = new User(02, "Aline", "Aline@gmail.com", "54998473277", "50024");
+
+		Order o1 = new Order(null, Instant.parse("2021-06-23T18:54:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2021-06-18T15:47:10Z"), OrderStatus.SHIPPED, u2);
+		Order o3 = new Order(null, Instant.parse("2021-06-21T05:25:14Z"), OrderStatus.CANCELED, u1);
+		
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        
+		
+		Payment pay1  = new Payment(null, Instant.parse("2021-06-23T20:54:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+        orderRepository.save(o1);
 	}
-	
+
 }
